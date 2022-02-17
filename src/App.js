00a2d4b85572cd68aea0c5caca7abc293198f764
Vu NS <ngsivu4599@gmail.com>
2022-02-17 13:33:32 +0700

    FrontEndTest

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import logoNCC from "./logoNCC.png";
+// import css-icon from "./css-icon.png"
+import css from "./css.png";
+import html from "./html.png";
+import url from "./url.png";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logoNCC} className="App-logo" />
+      <div className="body">
+        <div className="container-1">
+          <div className="text-1">Lorem ipsum dolor sit asmet?</div>
+          <div className="text-2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            tristique consequat placerat. Vestibulum auctor pellentesque sem, eu
+            posuere erat hendrerit quis. Maecenas vel consequat turpis. Nam
+            facilisis, ligula in mattis sodales, augue justo tristique nulla,
+            sed lacinia ante eros ut mi. Morbi vitae diam augue. Aliquam vel
+            mauris a nibh auctor commodo. Praesent et nisi eu justo eleifend
+            convallis. Quisque suscipit maximus vestibulum. Phasellus congue
+            mollis orci, sit amet luctus augue tristique eu. Donec vulputate
+            odio neque, sed semper turpis pellentesque a.
+          </div>
+        </div>
+      </div>
+      <div className="container-2">
+        <div className="container-html">
+          <div className="text-1">Lorem ipsum dolor sit amet</div>
+          <img src={css} className="html-logo" />
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non dui
+            sodales, faucibus libero ut, posuere felis. Donec imperdiet suscipit
+            accumsan. Aenean consequat condimentum velit ut tempor. Nam porta
+            massa in metus bibendum congue. Pellentesque ultrices liquam egestas
+            nunc at ullamcorper ultricies. Donec feugiat velit nulla, vel
+            sodales est ullamcorper id. Aenean consequat condimentum velit ut
+            tempor. Nam porta massa in metus bibendum congue. Pellentesque
+            ultrices vestibulum mattis.
+          </div>
+        </div>
+        <div>
+          <div className="text-1">Lorem ipsum dolor sit amet</div>
+          <img src={html} />
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non dui
+            sodales, faucibus libero ut, posuere felis. Donec imperdiet suscipit
+            accumsan. Aenean consequat condimentum velit ut tempor. Nam porta
+            massa in metus bibendum congue. Pellentesque ultrices liquam egestas
+            nunc at ullamcorper ultricies. Donec feugiat velit nulla, vel
+            sodales est ullamcorper id. Aenean consequat condimentum velit ut
+            tempor. Nam porta massa in metus bibendum congue. Pellentesque
+            ultrices vestibulum mattis.
+          </div>
+        </div>
+        <div>
+          <div className="text-1">Lorem ipsum dolor sit amet</div>
+          <img src={url} />
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non dui
+            sodales, faucibus libero ut, posuere felis. Donec imperdiet suscipit
+            accumsan. Aenean consequat condimentum velit ut tempor. Nam porta
+            massa in metus bibendum congue. Pellentesque ultrices liquam egestas
+            nunc at ullamcorper ultricies. Donec feugiat velit nulla, vel
+            sodales est ullamcorper id. Aenean consequat condimentum velit ut
+            tempor. Nam porta massa in metus bibendum congue. Pellentesque
+            ultrices vestibulum mattis.
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
